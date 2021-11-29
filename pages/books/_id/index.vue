b28a9f07b7e80e21bd427cdce61cac209292ed49
Adegbokan <adegbokanpaul@gmail.com>
2021-11-29 12:14:08 +0100

    new updates

---
+++ pages/books/_id/index.vue
@@ -0,0 +1,92 @@
+<template>
+    <main>
+        <v-item-group active-class="primary">
+            <v-container>
+                <div class="text-h6 text-left font-weight-medium grey--text mb-10 text-capitalize">{{bookTitle}}</span></div>
+                <v-row>
+                    <v-col  cols="12" md="6">
+                        <v-row>
+                            <v-col cols="12" md="4">
+                                <div class="fill-height d-flex justify-center flex-column align-center" >
+                                    <img src="/svg/login.svg" width="100%" alt="Login image"/>
+                                </div>
+                            </v-col>
+                            <v-col cols="12" md="8">
+                                <div class="grey--text mb-3">Author: <strong>Myles Munroe</strong></div>
+                                <div class="grey--text mb-3">Published on: <strong>21st of August, 2021</strong></div>
+                                <div class="grey--text mb-3">ISBN number: <strong>9875679679</strong></div>
+                                <div class="grey--text mb-3">Available copies: <strong>10</strong></div>
+                                <v-row>
+                                    <v-col cols="6">
+                                        <div>Borrow book for:</div>
+                                    </v-col>
+                                    <v-col>
+                                        <div class="grey--text"> 
+                                            <v-select
+                                                :items="items"
+                                                label="Days"
+                                                solo
+                                                :required="true"
+                                                ></v-select>
+                                        </div>
+                                    </v-col>
+                                </v-row>
+                                <div class="grey--text mb-6">Book description:
+                                    <p class="text-justify text--disabled">This is the description of this book. This is the description of this book. This is the description of this book.This is the description of this book This is the description of this book. This is the description of this book. This is the description of this book.This is the description of this book</p>
+                                </div>
+                                <v-btn @click="borrowBook" color="primary" block class="px-12 w-full bg-primary ">Borrow Book</v-btn>
+                            </v-col>
+                        </v-row>
+                    </v-col>
+                </v-row>
+                <v-dialog
+                    v-model="dialog"
+                    max-width="290"
+                    >
+                    <v-card>
+                        <v-card-title class="text-h5 text-center primary--text">
+                        Borrow Book
+                        </v-card-title>
+
+                        <v-card-text class="text-center">
+                       Are you sure you want to borrow this book for 2 days
+                        </v-card-text>
+
+                        <v-card-actions>
+                        <v-spacer></v-spacer>
+                            <v-btn @click="confirmBorrow" color="primary" depressed block class="px-12 w-full bg-primary ">Confirm</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                    </v-dialog>
+            </v-container>
+        </v-item-group>
+    </main>
+</template>
+<script>
+export default {
+  data(){
+      return {
+          bookTitle: 'Purpose Driven Life',
+          items: ['1 day', '2 days', '3 days'],
+          dialog: false
+      }
+  },
+  methods: {
+      borrowBook(){
+          this.dialog = true;
+      },
+      confirmBorrow(){
+          this.dialog = false;
+      }
+  }
+}
+</script>
+<style scoped>
+a{
+    text-decoration: none !important;
+}
+.table-width{
+    width: 100% !important;
+    background: red !important;
+}
+</style>
---
+++ pages/register.vue
@@ -0,0 +1,164 @@
+<template>
+  <v-app class="half-bg d-flex justify-center align-center">
+        <div class="d-flex d-flex justify-center justify-center pt-10 pb-10"  >
+            <img src="/svg/LMS.svg" width="5%" alt="Login image"/>
+        </div>
+        <v-card max-width="1000" class="d-flex flex-column mx-auto pb-10  relative bg-tertiary">
+            
+                    <v-form ref="form" v-model="valid" lazy-validation v-on:keyup.native.enter="signIn" >
+                        <v-row class="px-10 py-10">
+                            <v-col cols="12" md="6" >
+                                <div>
+                                    <v-text-field
+                                    v-model="register.firstName"
+                                    placeholder="First Name"
+                                    label="First Name"
+                                    dense
+                                    outlined
+                                    block
+                                    :required="true"
+                                    class="ma-0 p-0 "
+                                    />
+                                </div>
+                                <div>
+                                    <v-text-field
+                                    v-model="register.lastName"
+                                    placeholder="Last Name"
+                                    label="Last Name"
+                                    dense
+                                    outlined
+                                    block
+                                    :required="true"
+                                    class="ma-0 p-0 "
+                                    />
+                                </div>
+                                <div>
+                                    <v-text-field
+                                    v-model="register.email"
+                                    placeholder="Email"
+                                    label="Email"
+                                    dense
+                                    outlined
+                                    block
+                                    :required="true"
+                                    class="ma-0 p-0 "
+                                    />
+                                </div>
+                                <div>
+                                    <v-text-field
+                                    v-model="register.password"
+                                    placeholder="Password"
+                                    label="Password"
+                                    dense
+                                    outlined
+                                    block
+                                    :required="true"
+                                    class="ma-0 p-0 "
+                                    />
+                                </div>
+                                <div>
+                                    <v-text-field
+                                    v-model="register.confirmPassword"
+                                    placeholder="Re-enter Password"
+                                    label="Re-enter Password"
+                                    dense
+                                    outlined
+                                    block
+                                    :required="true"
+                                    class="ma-0 p-0 "
+                                    />
+                                </div>
+                            </v-col>
+                            <v-col cols="12" md="6" >
+                                <div>
+                                    <v-text-field
+                                    v-model="register.phoneNumber"
+                                    placeholder="Phone Number"
+                                    label="Phone Number"
+                                    dense
+                                    outlined
+                                    block
+                                    :required="true"
+                                    class="ma-0 p-0 "
+                                    />
+                                </div>
+                                <div>
+                                    <v-text-field
+                                    v-model="register.occupation"
+                                    placeholder="Occupation"
+                                    label="Occupation"
+                                    dense
+                                    outlined
+                                    block
+                                    :required="true"
+                                    class="ma-0 p-0 "
+                                    />
+                                </div>
+                                <div>
+                                    <v-text-field
+                                    v-model="register.address"
+                                    placeholder="Address"
+                                    label="Address"
+                                    dense
+                                    outlined
+                                    block
+                                    :required="true"
+                                    class="ma-0 p-0 "
+                                    />
+                                </div>
+                            </v-col>
+                        </v-row>
+                        <v-row class=" d-flex justify-center flex-column">
+                            <v-btn @onClick="signIn" color="primary" width="60%" class="bg-primary d-flex mx-auto">Register</v-btn>
+                            <nuxt-link to="/login" class="text-center mt-3">
+                                <span class="accent--text text-center">Already have an account?</span>
+                            </nuxt-link>
+                        </v-row>
+                    </v-form>
+        </v-card>
+  </v-app>
+</template>
+
+<script>
+// import { DEFAULT_RULE } from "@/constants/";
+export default {
+  components: {},
+  layout: "guest",
+  // middleware: "guest",
+  data: () => ({
+    dialog: true,
+    valid: false,
+    register: {
+      firstName: null,
+      lastName: null,
+      email: null,
+      confirmPassword: null,
+      phoneNumber: null,
+      occupation: null,
+      address: null,
+      password: null,
+    },
+  }),
+  computed: {
+    rule() {
+    //   return DEFAULT_RULE;
+    },
+  },
+  methods: {
+    async signIn() {
+      this.$router.push("/dashboard/")
+      }
+    },
+}
+</script>
+<style scoped>
+.half-bg {
+  background-color: #2B2862 !important;
+}
+.bg-tertiary{
+    background-color: #ffffff !important;
+}
+a{
+    text-decoration: none !important;
+}
+</style>
